3f8f7bc059d7ba5b3e53dfc1774612557a33aeaf
AravindDharavath <aravinddharavath4@gmail.com>
2026-09-25 16:54:55 +0530

    Initialized Express.js in project and request GET response

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,23 @@
-const product = require("./product");
-console.log(product);
+const express = require("express");
+const app = express();
+const products =[
+    {
+        id:1,
+        name:"Laptop",
+        price:60000
+    },
+    {
+        id:2,
+        name:"SmartPhone",
+        price:30000
+    }
+];
+app.get("/",(req,res) => {
+    res.send("Welcome to our E-Commerce Backend");
+})
+app.get("/api/products",(req,res) => {
+    res.json("Here all the products");
+})
+app.listen(5000 , () => {
+    console.log("Server running on http://localhost/5000");
+})
